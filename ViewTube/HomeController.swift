4a06c22d99ffecd066c078f4faaf7d5abfa0e8f7
Devesh Nema <deveshnema@deveshs-macbook-air.local>
2018-05-30 22:14:56 -0700

    1) Setup the initial HomeController as a collection view 2) Created the VideoCell - contains thumbnail image, profile image, title label and subtitle textview

---
+++ ViewTube/HomeController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ViewTube
+//
+//  Created by Devesh Nema on 5/30/18.
+//  Copyright © 2018 Devesh Nema. All rights reserved.
+//
+
+import UIKit
+
+fileprivate let cellid = "cellid"
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Home"
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: cellid)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellid, for: indexPath) as! VideoCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+
---
+++ ViewTube/VideoCell.swift
@@ -0,0 +1,100 @@
+//
+//  VideoCell.swift
+//  ViewTube
+//
+//  Created by Devesh Nema on 5/30/18.
+//  Copyright © 2018 Devesh Nema. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell : UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let thumbnailImageView : UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "taylor_thumbnail")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    let profileImageView : UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "taylor_profile")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.layer.cornerRadius = 22
+        return iv
+    }()
+    
+    let dividerLine : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Taylor Swift - Blank Space"
+        label.backgroundColor = UIColor.purple
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let subtitleTextView : UITextView = {
+        let tv = UITextView()
+        tv.backgroundColor = UIColor.green
+        tv.isEditable = false
+        tv.isScrollEnabled = false
+        tv.text = "TaylorSwiftVEVO · 2,296,114,694 views · Nov 10, 2014"
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    func setupViews() {
+        addSubview(thumbnailImageView)
+        addSubview(dividerLine)
+        addSubview(profileImageView)
+        addSubview(titleLabel)
+        addSubview(subtitleTextView)
+        
+        thumbnailImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16).isActive = true
+        thumbnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
+        thumbnailImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
+
+        profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 8).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+
+        titleLabel.topAnchor.constraint(equalTo: profileImageView.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+        subtitleTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        subtitleTextView.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 8).isActive = true
+        subtitleTextView.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor).isActive = true
+        subtitleTextView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        dividerLine.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 16).isActive = true
+        dividerLine.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        dividerLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        dividerLine.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        dividerLine.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+
+
+    }
+}
